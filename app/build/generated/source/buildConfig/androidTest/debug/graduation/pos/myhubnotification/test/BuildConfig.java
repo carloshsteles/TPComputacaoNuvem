2174becc3eec6691c0f003f7579a198f1abe19fe
Carlos Teles <carloshsteles@yahoo.com.br>
2017-07-12 14:21:28 -0300

    first commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/graduation/pos/myhubnotification/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package graduation.pos.myhubnotification.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "graduation.pos.myhubnotification.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
